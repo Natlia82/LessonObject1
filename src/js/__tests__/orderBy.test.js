4054c62fdf7d5b1d8c32b35b416d2d25679d5f48
Natalia <nata@host>
2021-08-11 06:57:36 +0500

    test 2

--- src/js/__tests__/orderBy.test.js
+++ src/js/__tests__/orderBy.test.js
@@ -13,3 +13,16 @@ test('one', () => {
     ]);
 
 });
+
+test('two', () => {
+    const obj = { name: 'мечник', health: 10, level: 2, attack: 80, defence: 40 };
+    const rezult = orderByProps(obj, ["name", "defence"]);
+    expect(rezult).toEqual([
+        { key: "name", value: "мечник" },
+        { key: "defence", value: 40 },
+        { key: "attack", value: 80 },
+        { key: "health", value: 10 },
+        { key: "level", value: 2 }
+    ]);
+
+});
